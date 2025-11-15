7d5e8076b1af1a219c33ea9c53008168eeeac7ee
Cristopher Mattheus Lima <mattheuslima0251@gmail.com>
2025-11-14 22:37:44 -0300

    service layer, component registration

--- src/main/java/com/cursojavanelio/curso/repositories/UserRepository.java
+++ src/main/java/com/cursojavanelio/curso/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cursojavanelio.curso.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
